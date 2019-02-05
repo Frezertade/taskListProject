ac94063c70870b3b15fd71a1b57ba07c91fd3fb6
mike <mikesol136@gmail.com>
2019-02-05 12:21:21 -0600

    user model is added

---
+++ src/main/java/model/User.java
@@ -0,0 +1,81 @@
+package model;
+
+public class User {
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String location;
+    private String password;
+
+    public User(int userID, String firstName, String lastName, String email,String phone, String location, String password) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.location = location;
+        this.password = password;
+        this.email=email;
+    }
+
+    public User(){}
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
