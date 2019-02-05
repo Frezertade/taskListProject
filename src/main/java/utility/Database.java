89f5bf0e83edf8af155e682022a3b1c9c1130182
mike <mikesol136@gmail.com>
2019-02-05 17:02:22 -0600

    Temporary database class and DB names are created

---
+++ src/main/java/utility/Database.java
@@ -0,0 +1,34 @@
+package utility;
+
+import util.DBName;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Database<T> {
+
+    private static Database single_instance = null;
+    private  HashMap<DBName, List<T>> tables;
+    private Database(){
+       tables = new HashMap<>();
+    }
+
+    public static Database getInstance(){
+        if(single_instance == null){
+            single_instance = new Database();
+        }
+
+        return single_instance;
+    }
+
+    public void setValue(DBName name, T value){
+        List<T> values= tables.get(name);
+        if(values== null) values= new ArrayList<>();
+        values.add(value);
+    }
+
+    public List<T> getValue(DBName name){
+        return tables.get(name);
+    }
+}
