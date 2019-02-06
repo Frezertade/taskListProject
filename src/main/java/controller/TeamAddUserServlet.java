f7fcbf2fae2a4ddb509c2efce7bef5b6298a4e9a
frezer tadesse <frezertade@gmail.com>
2019-02-05 22:29:30 -0600

    add user to team as members

--- src/main/java/controller/TeamAddUserServlet.java
+++ src/main/java/controller/TeamAddUserServlet.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@WebServlet(urlPatterns = "/addUsetTeam")
+@WebServlet(urlPatterns = "/addUserTeam")
 public class TeamAddUserServlet extends HttpServlet {
 
     Database<User> dbUser = Database.getInstance();
@@ -32,35 +32,29 @@ public class TeamAddUserServlet extends HttpServlet {
             int userId = Integer.parseInt(request.getParameter("userId"));
 
             //if both user and Team parameters are found add them in to database
-            List <User> users = dbUser.getValue(DBName.USER);
-            List <Team> teams = dbTeam.getValue(DBName.TEAM);
-            Optional user = users.stream().filter(user1 -> user1.getUserID()==userId).findFirst();
-            Optional team = teams.stream().filter(team1 -> team1.getTeamId()==teamId).findFirst();
-            if (user.isPresent()&&team.isPresent()) {
-                dbTeamMemeber.setValue(DBName.TEAM_MEMBER, new TeamMember(dbTeam.getValue(DBName.TEAM).get(1),dbUser.getValue(DBName.USER).get(1)));
+            List<User> users = dbUser.getValue(DBName.USER);
+            List<Team> teams = dbTeam.getValue(DBName.TEAM);
+            Optional user = users.stream().filter(user1 -> user1.getUserID() == userId).findFirst();
+            Optional team = teams.stream().filter(team1 -> team1.getTeamId() == teamId).findFirst();
+            if (user.isPresent() && team.isPresent()) {
+                dbTeamMemeber.setValue(DBName.TEAM_MEMBER, new TeamMember(dbTeam.getValue(DBName.TEAM).get(1), dbUser.getValue(DBName.USER).get(1)));
+
+                String JSONteamMemberList;
+                List<TeamMember> teamMemberList = dbTeamMemeber.getValue(DBName.TEAM_MEMBER);
+                JSONteamMemberList = new Gson().toJson(teamMemberList);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                out.write(JSONteamMemberList);
+            } else {
+                String JSONteamNotFound = "{\"status\" :\" failure to add \",\"response\" : \"user or team dose not exist\"}";
+                String Json = new Gson().toJson(JSONteamNotFound);
+
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                out.write(Json);
             }
-
-
-
-
         }
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
          doPost(request,response);
-            //   PrintWriter out = response.getWriter();
-//
-//            dbUser.setValue(DBName.USER, new User(1,"FREZER","TADESSE","FREE@mum.edu","432-234-2345","fairfield,iowa,52557","password"));
-//            dbUser.setValue(DBName.USER, new User(2,"Gebere","MANOFTHEYEAR","GEREe@mum.edu","510-234-2345","fairfield,iowa,52557","password") );
-//
-//            dbTeam.setValue(DBName.TEAM,new Team(1,"Programers",dbUser.getValue(DBName.USER).get(0)));
-//            dbTeam.setValue(DBName.TEAM,new Team(1,"Programers",dbUser.getValue(DBName.USER).get(1)));
-//
-//
-//            String JSONteams;
-//            List<Team> teamList = dbTeam.getValue(DBName.TEAM);
-//            JSONteams = new Gson().toJson(teamList);
-//
-//            response.setContentType("application/json");
-//            response.setCharacterEncoding("UTF-8");
-//            out.write(JSONteams);
         }
 }
