f05b54cb37550efe83065e0662d8cbddd905a530
frezer tadesse <frezertade@gmail.com>
2019-02-06 12:24:21 -0600

    COMMENTED

--- src/main/java/controller/TeamServlet.java
+++ src/main/java/controller/TeamServlet.java
@@ -24,7 +24,7 @@ public class TeamServlet extends HttpServlet {
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             PrintWriter out = response.getWriter();
 
-           //respondes the given data to the caller
+           //GET TEAM SERVICE AND RESPOND TEAMS FROM DATA
             TeamService teamServe = new TeamService();
             String JSONteams;
             List<Team> teamList = teamServe.teams();
